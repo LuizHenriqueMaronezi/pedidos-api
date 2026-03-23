810c943b811002d8fceaf31740f0cb2c6144c32c
LuizHenriqueMaronezi <luizhenriquemaronezi04@gmail.com>
2026-03-23 19:10:09 -0300

    Primeiro commit: Estrutura inicial Spring Boot e REST testado

---
+++ pedidos-api/src/main/java/io/github/LuizHenriqueMaronezi/pedidosapi/PedidosApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.LuizHenriqueMaronezi.pedidosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PedidosApiApplication.class, args);
+	}
+
+}
---
+++ pedidos-api/src/main/java/io/github/LuizHenriqueMaronezi/pedidosapi/controller/CategoriaController.java
@@ -0,0 +1,15 @@
+package io.github.LuizHenriqueMaronezi.pedidosapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+    @GetMapping
+    public String listar(){
+        return "REST está funcionando!";
+    }
+}
